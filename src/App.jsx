6b0760ce622f693bc31c33055defecf4843cf22c
jrshields86 <jrshields86@gmail.com>
2024-09-26 19:02:20 -0600

    picking it back up

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <h1>Hello World</h1>
+      <h1>My name is Joe</h1>
+    </>
+  )
+}
+
+export default App
